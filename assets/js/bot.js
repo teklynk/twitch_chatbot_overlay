d1a96174f1c62a97537725fad8495ec75db3d06a
teklynk <ryanjones153@gmail.com>
2021-08-21 17:29:26 -0400

    added support for emotes. reformat code

--- assets/js/bot.js
+++ assets/js/bot.js
@@ -3,7 +3,7 @@ function getUrlParameter(name) {
     let regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
     let results = regex.exec(location.search);
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-};
+}
 
 let botName = getUrlParameter('bot');
 let channelName = getUrlParameter('channel');
@@ -21,9 +21,57 @@ function timerIncrement() {
     if (idleTime === 600) { //600 seconds = 10mins
 
         document.getElementById('imgSrc').src = 'assets/images/robot-sleep.gif';
-        
+
     }
 
+}
+
+function htmlEntities(html) {
+    function it() {
+        return html.map(function (n, i, arr) {
+
+            if (n.length === 1) {
+                return n.replace(/[\u00A0-\u9999<>\&]/gim, function (i) {
+                    return '&#' + i.charCodeAt(0) + ';';
+                });
+            }
+
+            return n;
+        });
+    }
+
+    let isArray = Array.isArray(html);
+
+    if (!isArray) {
+        html = html.split('');
+    }
+
+    html = it(html);
+
+    if (!isArray) html = html.join('');
+
+    return html;
+}
+
+function formatEmotes(text, emotes) {
+    let splitText = text.split('');
+    for (let i in emotes) {
+        let e = emotes[i];
+        for (let j in e) {
+            let mote = e[j];
+            if (typeof mote == 'string') {
+                mote = mote.split('-');
+                mote = [parseInt(mote[0]), parseInt(mote[1])];
+                let length = mote[1] - mote[0],
+                    empty = Array.apply(null, new Array(length + 1)).map(function () {
+                        return ''
+                    });
+                splitText = splitText.slice(0, mote[0]).concat(empty).concat(splitText.slice(mote[1] + 1, splitText.length));
+                splitText.splice(mote[0], 1, '<img class="emoticon" src="http://static-cdn.jtvnw.net/emoticons/v1/' + i + '/3.0">');
+            }
+        }
+    }
+    return htmlEntities(splitText).join('')
 }
 
 const client = new tmi.Client({
@@ -35,48 +83,50 @@ client.connect();
 
 let idleTime = 0;
 let msgCount = 0;
+let showEmotes = true;
 
 client.on('message', (channel, tags, message, self) => {
 
     let chatname = `${tags['display-name']}`;
     let chatmessage = message.replace(/(<([^>]+)>)/ig, "");
-    
+    let chatemotes = tags.emotes;
+
     msgCount = msgCount + 1;
-    
+
     if (chatname === botName) {
-    
+
         idleTime = 0;
         timer = 0;
         clearInterval(timer);
-  
-        document.getElementById('notif').innerHTML = chatmessage;
+
+        document.getElementById('notif').innerHTML = showEmotes ? formatEmotes(chatmessage, chatemotes) : htmlEntities(chatmessage);
         document.getElementById('notif').classList.add('visible');
         document.getElementById('imgSrc').classList.add('visible');
-        
-        if (msgCount % 5 == 0) {
+
+        if (msgCount % 5 === 0) {
             document.getElementById('imgSrc').src = 'assets/images/robot-fire.gif';
         } else {
             document.getElementById('imgSrc').src = 'assets/images/robot-run.gif';
         }
-        
-        setTimeout(function() { 
+
+        setTimeout(function () {
             document.getElementById('notif').innerHTML = '';
             document.getElementById('notif').classList.remove('visible');
             document.getElementById('imgSrc').classList.remove('visible');
-            document.getElementById('imgSrc').src = 'assets/images/robot.gif'; 
+            document.getElementById('imgSrc').src = 'assets/images/robot.gif';
         }, 5500);
-        
+
     } else {
-  
+
         idleTime = 0;
         timer = 0;
         clearInterval(timer);
-        
+
         document.getElementById('notif').innerHTML = '';
         document.getElementById('notif').classList.remove('visible');
         document.getElementById('imgSrc').classList.remove('visible');
         document.getElementById('imgSrc').src = 'assets/images/robot.gif';
-        
+
     }
 
 });
